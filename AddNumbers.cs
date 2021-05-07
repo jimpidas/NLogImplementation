1d4c2f21eda8b6c6f707b825c8bf9a87fde729bc
unknown <jimpidas>
2021-05-07 12:10:36 +0530

    [Jyosmita]: Added the code to calculate two numbers using NLog

---
+++ AddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogImplementation
+{
+    public class AddNumbers
+    {
+        NLog nlog = new NLog();
+        public int Sum(int a, int b)
+        {
+            if(a==0 || b==0)
+            {
+                nlog.LogDebug("Debug Successfull : sum()");
+                nlog.LogError("Expecting null values");
+                nlog.LogWarn("a or b should not equal to 0");
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug Successfull");
+            return c;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NLogImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition problem \n Enter a,b values");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("Result is " + " " + add.Sum(a,b));
+        }
+    }
+}
